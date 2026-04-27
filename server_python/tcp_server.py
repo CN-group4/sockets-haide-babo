943b9c5ce70b6f6ce94e69aa94d07bc4034c282e
emmaghita <ghitaemmadaria@gmail.com>
2026-04-27 11:00:37 +0300

    received strings correctly

--- server_python/tcp_server.py
+++ server_python/tcp_server.py
@@ -9,37 +9,36 @@ def main():
     server_socket.bind((HOST, PORT))
     server_socket.listen(1)
 
-    print(f"[TCP Server] Listen on port {PORT}...")
+    print(f"[TCP Server] Ascult pe portul {PORT}...")
     conn, addr = server_socket.accept()
-    print(f"[TCP Server] Connected with {addr}")
+    print(f"[TCP Server] Conectat cu {addr}")
 
     try:
         while True:
-            # message from client received
             data = conn.recv(1024)
             if not data:
-                print("[TCP Server] Connection was closed by client.")
+                print("[TCP Server] Conexiunea a fost închisă de client.")
                 break
 
-            mesaj_primit = data.decode('utf-8')
+            mesaj_primit = data.decode('utf-8').strip()
             print(f"[Client]: {mesaj_primit}")
 
-            if mesaj_primit.strip().lower() == 'exit':
-                print("[TCP Server] Client sent 'exit'. Closing connection.")
+            if mesaj_primit.lower() == 'exit':
+                print("[TCP Server] Clientul a trimis 'exit'. Închid conexiunea.")
                 break
 
-            # send answer
-            raspuns = input("[You (Server)]: ")
-            conn.sendall(raspuns.encode('utf-8'))
+            
+            raspuns = input("[Tu (Server)]: ")
+            conn.sendall((raspuns + "\n").encode('utf-8'))
 
             if raspuns.strip().lower() == 'exit':
-                print("[TCP Server] You sent 'exit'. Closing connection.")
+                print("[TCP Server] Ai trimis 'exit'. Închid conexiunea.")
                 break
 
     finally:
         conn.close()
         server_socket.close()
-        print("[TCP Server] Socket closed.")
+        print("[TCP Server] Socket închis.")
 
 if __name__ == '__main__':
     main()
